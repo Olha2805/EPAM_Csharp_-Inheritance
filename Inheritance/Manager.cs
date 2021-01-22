cc760fc6d611e604841bc70d1bca529c71dc021a
Olha Bohatova <torshynina@gmail.com>
2021-01-22 13:07:08 +0000

    Update Manager.cs

--- Inheritance/Manager.cs
+++ Inheritance/Manager.cs
@@ -7,14 +7,14 @@ namespace InheritanceTask
     public class Manager : Employee
     {
         private readonly int quantity;
-         private readonly int clientAmount;
+        private readonly int clientAmount;
         public Manager(string name, decimal salary, int clientAmount) : base(name, salary)
         {
             Name = name;
             Salary = salary;
             this.clientAmount = clientAmount;
         }
-        public void SetBonus(decimal bonus, int clientAmount)   // public override virtual SetBonus(decimal bonus, int clientAmount)
+        public override void SetBonus(decimal bonus)
         {
             if (clientAmount > 100 && clientAmount <= 150) this.bonus += 500;
             else if (clientAmount > 150) this.bonus += 1000;
