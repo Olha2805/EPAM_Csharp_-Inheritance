fb831056896beea6d8dd774f17542cc1892300b3
Olha Bohatova <torshynina@gmail.com>
2021-01-23 08:08:11 +0000

    Update Company.cs

--- Inheritance/Company.cs
+++ Inheritance/Company.cs
@@ -1,1 +1,49 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace InheritanceTask
+{
+    public class Company
+    {
+        private readonly Employee[] employees;
+        public Company(Employee[] employee)
+        {
+            employees = employee;
+        }
+        public void GiveEverybodyBonus(decimal companyBonus)
+        {
+            for(int i = 0; i < employees.Length; i++)
+            {
+                employees[i].SetBonus(companyBonus);
+            }
+        }
+
+        public decimal TotalToPay()
+        {
+            decimal total_salary = 0;
+            for (int i = 0; i < employees.Length; i++)
+            {
+                total_salary += employees[i].ToPay();
+            }
+            return total_salary;
+        }
+
+        public string NameMaxSalary()
+        {
+            string NameMaxSalary = employees[0].Name;
+            decimal maxSalary = employees[0].ToPay();
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].Salary > maxSalary)
+                {
+                    maxSalary = employees[i].ToPay();
+                    string nameMaxSalary = employees[i].Name;
+                }
+            }
+            return NameMaxSalary;
+        }
+    }
+    
+}
